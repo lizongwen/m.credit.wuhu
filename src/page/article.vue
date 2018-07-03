ba6070cdbee11090fd275e71fda0eb8e1961fb3c
crystal <1525978952@qq.com>
2018-07-03 11:48:11 +0800

    1

--- src/page/article.vue
+++ src/page/article.vue
@@ -1,13 +1,124 @@
 <template>
-	<div>全文检索文章列表</div>
+	<div class="article">
+		<div class="article-header">站内检索</div>
+		<div class="information">共搜索到<span>5</span>条结果</div>
+		<div>
+			<van-cell v-for="(item,index) in list" :key="index"  :to="{ path: 'articleDetail', query: { id: item.id }}">
+				<template slot="title">
+                    <div class="van-ellipsis news-title fl">惠国征信服务股份有限公司</div>
+					<div class="companycreditCode fr">2018/02/01</div>
+				</template>
+			</van-cell>
+		</div>
+	</div>
 </template>
 
 <script>
-export default {
-
-}
+	export default {
+		data() {
+			return {
+				loading: false,
+				list: [{
+						title: "标题一标题一标题一标题一标题一标题一标题一",
+						content: "标题内容一",
+						date: "2018/01/05"
+					},
+					{
+						title: "标题二",
+						content: "标题内容二",
+						date: "2018/01/05"
+					},
+					{
+						title: "标题三",
+						content: "标题内容三",
+						date: "2018/01/05"
+					},
+					{
+						title: "标题四",
+						content: "标题内容四",
+						date: "2018/01/05"
+					},
+					{
+						title: "标题五",
+						content: "标题内容五",
+						date: "2018/01/05"
+					},
+					{
+						title: "标题五",
+						content: "标题内容五",
+						date: "2018/01/05"
+					},
+					{
+						title: "标题五",
+						content: "标题内容五",
+						date: "2018/01/05"
+					},
+					{
+						title: "标题五",
+						content: "标题内容五",
+						date: "2018/01/05"
+					},
+					{
+						title: "标题五",
+						content: "标题内容五",
+						date: "2018/01/05"
+					},
+					{
+						title: "标题五",
+						content: "标题内容五",
+						date: "2018/01/05"
+					},
+					{
+						title: "标题五",
+						content: "标题内容五",
+						date: "2018/01/05"
+					},
+					{
+						title: "标题五",
+						content: "标题内容五",
+						date: "2018/01/05"
+					},
+					{
+						title: "标题五",
+						content: "标题内容五",
+						date: "2018/01/05"
+					},
+					{
+						title: "标题五",
+						content: "标题内容五",
+						date: "2018/01/05"
+					}
+				]
+			};
+		},
+		methods: {
+			onLoad() {
+				setTimeout(() => {
+					this.list.push({
+						title: "标题一",
+						content: "标题内容一"
+					});
+					this.loading = false;
+				}, 2000);
+			}
+		}
+	};
+</script>
 </script>
 
 <style lang="scss" scoped>
-
+	.article {
+		font-size: 14px;
+		.article-header {
+			width: 64px;
+			margin: 15px auto;
+			font-size: 16px;
+			color: #E23B41;
+		}
+		.information {
+			padding: 0 0 10px 10px;
+			font-size: 12px;
+			color: rgba(46, 46, 46, 0.4);
+		}
+	}
 </style>
--- src/page/articleDetail.vue
+++ src/page/articleDetail.vue
@@ -1,14 +1,55 @@
 <template>
-	<div>文章详情</div>
+	<div class="articleDetail">
+		<div class="article-title">“惠政平台”助力芜湖信用示范城市建设</div>
+		<div class="article-date">
+			<div class="van-ellipsis news-title fl">发布时间: 2017/03/08</div>
+			<div class="companycreditCode fl">来自: 芜湖新闻网-芜湖日报</div>
+		</div>
+		<div class="hr"></div>
+		<div class="article-content">
+			<p>记者近日从市发改委获悉，当前惠国征信正在着力打造“ 惠政平台”，该平台将应用于市场监管、公共服务、财政 补贴、税收征缴和政府采购等多个政府职能领域，助力芜 湖实现创建国家信用体系建设示范城市的目标。
+			</p>
+			<p>记者近日从市发改委获悉，当前惠国征信正在着力打造“ 惠政平台”，该平台将应用于市场监管、公共服务、财政 补贴、税收征缴和政府采购等多个政府职能领域，助力芜 湖实现创建国家信用体系建设示范城市的目标。
+			</p>
+			<p>记者近日从市发改委获悉，当前惠国征信正在着力打造“ 惠政平台”，该平台将应用于市场监管、公共服务、财政 补贴、税收征缴和政府采购等多个政府职能领域，助力芜 湖实现创建国家信用体系建设示范城市的目标。
+			</p>
+			<p>记者近日从市发改委获悉，当前惠国征信正在着力打造“ 惠政平台”，该平台将应用于市场监管、公共服务、财政 补贴、税收征缴和政府采购等多个政府职能领域，助力芜 湖实现创建国家信用体系建设示范城市的目标。
+			</p>
+		</div>
+	</div>
 </template>
 
 <script>
-export default {
+	export default {
 
-}
+	}
 </script>
 
 <style lang="scss" scoped>
-
+	.articleDetail {
+		.article-title {
+			margin: 10px;
+		}
+		.article-date {
+			overflow: hidden;
+			padding: 0px 10px;
+			font-size: 12px;
+			color: rgba(46, 46, 46, 0.4);
+			.companycreditCode {
+				margin: 0 0 15px 20px;
+			}
+		}
+		.hr {
+			border-bottom: 1px solid #EBEBEB;
+		}
+		.article-content {
+			padding: 10px;
+			font-size: 14px;
+			color: rgba(0,0,0,0.8);
+			p{
+				line-height: 20px;
+				margin-bottom: 20px;
+			}
+		}
+	}
 </style>
-
